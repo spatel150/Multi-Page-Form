e7bc632411753c74a3609aab275f3216586c9d6f
Samidh Patel <45617027+spatel150@users.noreply.github.com>
2019-10-20 13:23:01 -0400

    Add files via upload

---
+++ src/components/Success.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+export class Success extends Component {
+    continue = e => {
+        e.preventDefault();
+        // PROCESS FORM // 
+        this.props.nextStep()
+    };
+
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();
+    };
+
+    render() {
+        return (
+            <MuiThemeProvider>
+               <React.Fragment>
+                   <AppBar title="Success" />
+                    <h1>Thank You For Your Submission</h1>
+                    <p>You will get an email with further instructions</p>
+               </React.Fragment> 
+            </MuiThemeProvider>
+        )
+    }
+}
+
+const styles = {
+    button: {
+        margin: 15
+    }
+}
+
+export default Success
